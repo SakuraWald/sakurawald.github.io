6992c5e6dfe30034e251885c7e4290f0921c62db
SakuraWald <sakurawald@gmail.com>
2023-01-23 09:53:29 +0800

    save

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+echo "Saving current theme..."
+cd ./themes/toxic 
+git add .
+git commit -m "save"
+git push origin HEAD:dev
+echo "Saving current blog..."
+cd ../..
+git add .
+git commit -m "save"
+git push
